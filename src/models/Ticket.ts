68ebbbe1ea86a1e2a421e0169cf1cd85f8c686a9
Kangonga <guchu413@gmail.com>
2023-04-28 15:48:55 +0300

    feat: extend the ticket model to include a one to many relationship with the admin, developer and agent models

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,24 +1,35 @@
-import mongoose, { Schema, model, Document } from  'mongoose'
+import mongoose, { Schema, model, Document, Types } from  'mongoose'
 
 //define interface for user object
 interface Ticket {
     title: String;
     description: String;
-    agent: String;
-    developer: String;
     source: String;
     priority: String;
     isClosed: Boolean;
     createdAt: String;
     closedAt: String;
+    admin: {
+        type: Types.ObjectId,
+        ref:String
+    };
+    developer: {
+        type: Types.ObjectId,
+        ref:String
+    };
+    agent: {
+        type: Types.ObjectId,
+        ref:String
+    };
 }
 
 //create mongoose schema for user object
 const ticketSchema = new Schema<Ticket>({
     title:{ type:String, required:true },
     description: { type: String, required: true },
-    agent: { type: String, required: true },
-    developer: { type: String, required: true },
+    agent: { type: Types.ObjectId, required: true },
+    developer: { type: Types.ObjectId, required: true },
+    admin: { type: Types.ObjectId, required: true },
     source: { type: String, required: true },
     priority: { type: String, required: true },
     isClosed: { type: Boolean, required: true },
