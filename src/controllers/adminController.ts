e5eb96551eea319e5761a6e46ca9e76f02a20c84
Kangonga <guchu413@gmail.com>
2023-05-03 18:35:23 +0300

    fix: remove errors on the admin controller crud operations

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -14,19 +14,10 @@ export const AdminController = {
   },
   
   async createadmin(req: Request, res: Response) {
-    const admin = new Admin({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      agents: req.body.agents,
-      tickets: req.body.tickets,
-      developers: req.body.developers
-    });
-
     try {
-      const newadmin = await admin.save();
-      res.status(201).json(newadmin);
+      const admin = await Admin.create(req.body)
+      console.log(req.body)
+      res.status(201).json(admin);
     } catch (err) {
       res.status(400).json({ message: err });
     }
@@ -46,15 +37,20 @@ export const AdminController = {
 
   async updateadminById(req: Request, res: Response) {
     try {
-      const admin = await Admin.findById(req.params.id);
+      const adminId = req.params.id
+      const admin = await Admin.findOneAndUpdate({
+        _id:adminId
+      },
+      req.body,
+      {
+        new:true,
+        runValidators:true
+      }
+      );
       if (!admin) {
         return res.status(404).json({ message: 'admin not found' });
       }
-
-      // additional fields for admin model
-
-      const updatedadmin = await admin.save();
-      res.json(updatedadmin);
+      res.json(admin);
     } catch (err) {
       res.status(400).json({ message: err });
     }
@@ -62,11 +58,13 @@ export const AdminController = {
 
   async deleteadminById(req: Request, res: Response) {
     try {
-      const admin = await Admin.findById(req.params.id);
+      const adminId = req.params.id
+      const admin = await Admin.findOneAndDelete({
+        _id:adminId
+      });
       if (!admin) {
         return res.status(404).json({ message: 'admin not found' });
       }
-    //   await admin.remove();
       res.json({ message: 'admin deleted successfully' });
     } catch (err) {
       res.status(500).json({ message: err });
