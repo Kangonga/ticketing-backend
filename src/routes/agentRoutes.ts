f0011ce6207f5879d7fc1b5f16f65cee0d6fc2bc
Kangonga <guchu413@gmail.com>
2023-05-04 21:53:38 +0300

    fix: rename agentController to AgentController

--- src/routes/agentRoutes.ts
+++ src/routes/agentRoutes.ts
@@ -1,16 +1,16 @@
 import express, { Express, Request, Response, Router } from 'express'
-// import { createAgent, deleteAgent, getAllAgents, getOneAgent, updateOneAgent } from '../controllers/agentController.js';
-import { agentController } from '../controllers/agentController.js'; 
+// import { createAgent, deleteAgent, getAllAgents, getOneAgent, updateOneAgent } from '../controllers/AgentController.js';
+import { AgentController } from '../controllers/agentController.js'; 
 
 const agentRoutes = Router();
 
 agentRoutes.route('/')
-.get(agentController.getAllAgents)
-.post(agentController.createAgent)
+.get(AgentController.getAllAgents)
+.post(AgentController.createAgent)
 
 agentRoutes.route('/:id')
-.get(agentController.getAgentById)
-.patch(agentController.updateAgentById)
-.delete(agentController.deleteAgentById)
+.get(AgentController.getAgentById)
+.patch(AgentController.updateAgentById)
+.delete(AgentController.deleteAgentById)
 
 export default agentRoutes
