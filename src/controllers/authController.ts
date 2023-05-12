66ac3ae1567a46db6868eeb8295ee2fd28cde6db
Kangonga <guchu413@gmail.com>
2023-05-13 00:54:05 +0300

    test: add test actions to controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,12 @@
-export const signIn = ()=>{
+import { Request, Response } from "express"
 
+
+export const signIn = (req:Request, res:Response)=>{
+    console.log(req.sessionID)
+    res.send(req.session)
 }
 export const signOut = ()=>{
-
+    
 }
 export const signUp = ()=>{
 
