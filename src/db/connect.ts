614b0b82cd51ac61ec324e651bb6566c7dec83a1
Kangonga <guchu413@gmail.com>
2023-05-13 00:53:35 +0300

    feat: create configuration for the session store

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import MongoStore from "connect-mongo";
 import dotenv from 'dotenv'
+import session from "express-session";
+import { default as connectMongoDBSession }  from "connect-mongodb-session"
 
+const MongoDBStore = connectMongoDBSession(session);
 
 dotenv.config()
 
@@ -9,14 +12,14 @@ export const connectDb = (url:any)=>{
     return mongoose.connect(url)
 }
 
-const sessionStore = MongoStore.create({
-    mongoUrl:process.env.MONGODB_URI,
-    ttl:20000
-})
-
 export const sessionOptions = {
     secret: process.env.SESSION_SECRET as string,
     cookie: { maxAge:20000, httpOnly: true, signed: true},
     resave: false,
-    store: sessionStore
 }
+
+export const sessionStore = new MongoDBStore({
+    collection:'mySessions',
+    uri:process.env.MONGODB_URI as string,
+})
+
