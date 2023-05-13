1e4a388cf18cbecf7f02faa86829554958e46446
Kangonga <guchu413@gmail.com>
2023-05-13 22:55:43 +0300

    chore: delete unused session options

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -11,12 +11,6 @@ export const connectDb = (url:any)=>{
     return mongoose.connect(url)
 }
 
-export const sessionOptions = {
-    secret: process.env.SESSION_SECRET as string,
-    cookie: { maxAge:20000, httpOnly: true, signed: true},
-    resave: false,
-}
-
 export const sessionStore = new MongoDBStore({
     collection:'mySessions',
     uri:process.env.MONGODB_URI as string,
